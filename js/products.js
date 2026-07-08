fe9309cd52054aab133bbae3f9d499213e48e455
Chris Sompa <77889035+christophersompa@users.noreply.github.com>
2026-07-08 10:22:30 +0100

    product js added

--- js/products.js
+++ js/products.js
@@ -247,7 +247,7 @@ const SITE_IMAGES = {
 
   /* category tile imagery */
   catNewArrivals: "images/products/new_arrivals.png",
-  catClothing:    "images/products/activewear.png",
+  catClothing:    "images/products/products/activewear.png",
   catFood:        ph("Food",         { w: 600, h: 600 }),
   catSkincare:    ph("Skincare",     { w: 600, h: 600 }),
   catAccessories: ph("Accessories",  { w: 600, h: 600 })
